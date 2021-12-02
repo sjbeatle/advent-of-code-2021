f187601443dad05ffd33ee86a5ea064290c44408
Steven Black <sblack@us.ibm.com>
2021-12-02 12:46:46 -0500

    more elegant solution day 1 part 2

--- src/day/01/b/index.ts
+++ src/day/01/b/index.ts
@@ -4,12 +4,9 @@ import {
 } from './input';
 
 let inc = 0;
-const slidingMeasurement: number[] = [];
 
-for (let index = 2; index < measurements.length; index++) {
-  const m = measurements[index - 2] + measurements[index - 1] + measurements[index];
-  slidingMeasurement.push(m);
-  if (m > slidingMeasurement[index - 3]) inc++
+for (let index = 3; index < measurements.length; index++) {
+  if (measurements[index] > measurements[index - 3]) inc++
 }
 
 console.log(inc, ' sliding measurements are larger than the previous measurement');
