7df0cfff99520b60ab46630a16f85c8cc5b0d1fa
Steven Black <sblack@us.ibm.com>
2021-12-09 07:34:31 -0500

    fix day 6 bug

--- src/day/06/b/index.ts
+++ src/day/06/b/index.ts
@@ -16,7 +16,7 @@ for (let i = 0; i <= childTimer; i++) {
 timers.forEach(t => timerCounts[t] = ++timerCounts[t]);
 
 for (let i = 0; i < days; i++) {
-  let creators = timerCounts['0'];
+  let creators = timerCounts[0];
   Object.keys(timerCounts).map(k => parseInt(k, 10)).sort().forEach( // sort may not be necessary, but doesn't hurt
     t => {
       if (t + 1 > childTimer)
